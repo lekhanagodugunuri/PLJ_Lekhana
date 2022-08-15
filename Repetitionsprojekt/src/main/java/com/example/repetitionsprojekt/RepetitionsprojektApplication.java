6075e6686bdf521879598079a8c43706cc4b06c6
lekhanagodugunuri <lekhana.godugunuri@noseryoung.com>
2022-08-15 16:57:39 +0200

    working on front end

---
+++ Repetitionsprojekt/src/main/java/com/example/repetitionsprojekt/RepetitionsprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.repetitionsprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepetitionsprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepetitionsprojektApplication.class, args);
+    }
+
+}
