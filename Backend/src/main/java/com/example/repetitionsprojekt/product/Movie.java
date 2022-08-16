1bb1e706122692f13651b2a8a446afd641519b57
lekhanagodugunuri <lekhana.godugunuri@noseryoung.com>
2022-08-16 13:43:43 +0200

    started with backend

---
+++ Backend/src/main/java/com/example/repetitionsprojekt/product/Movie.java
@@ -0,0 +1,28 @@
+package com.example.repetitionsprojekt.product;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "movie_id")
+    private Integer movieId;
+    @NotNull
+    @Column(name = "movie_name")
+    private String movieName;
+
+    @Column(name = "movie_price")
+    private Integer moviePrice;
+
+    @Column(name = "movie_genre")
+    private Double movieGenre;
+}
---
+++ Backend/src/main/java/com/example/repetitionsprojekt/product/MovieService.java
@@ -0,0 +1,4 @@
+package com.example.repetitionsprojekt.product;
+
+public class MovieService {
+}
